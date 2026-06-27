98cf18c25b788e84d7137f055899e0edf61a2cb7
Verkiel-cmd <ezekielU675@gmail.com>
2026-06-27 11:12:49 +0800

    Security Update Attemmpt 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import config from './auth_section/config';
 import Frontlog from './auth_section/Frontlog';
 import ListStud from './Student_lists/ListStud';
 import CreateStudent from './Student_lists/CreateStudent';
